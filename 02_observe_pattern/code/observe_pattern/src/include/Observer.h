0a93b3d36fc73a18f3a8c84a1b0cfcf7901c0ab7
liubo00 <liubo@zerozero.cn>
2025-04-16 22:15:14 +0800

    kglksjdjlgjj

--- 02_observe_pattern/code/observe_pattern/src/include/Observer.h
+++ 02_observe_pattern/code/observe_pattern/src/include/Observer.h
@@ -7,7 +7,7 @@
  * |  V1.0.1    2024-09-06
  * ---------------------------------------
  * | @brief   : three observers
- * |            Observer1、Observer2、Observer3
+ *              Observer1、Observer2、Observer3
  ******************************************************************************/
 #include<iostream>
 
@@ -17,28 +17,28 @@ using std::endl;
 
 class Observer
 {
-  public:
-    //处理消息的接口
+public:
+    // 处理消息的接口
     virtual void handle(int msgid) = 0;
 };
 
-//第一个观察者实例
-class Observer1 :public Observer
+// 第一个观察者实例
+class Observer1 : public Observer
 {
   public:
     void handle(int msgid)
     {
         switch (msgid)
         {
-            case 1:
-                cout << "Observer1 recv 1 msg!" << endl;
-                break;
-            case 2:
-                cout << "Observer1 recv 2 msg!" << endl;
-                break;
-            default:
-                cout << "Observer1 recv unknow msg!" << endl;
-                break;
+        case 1:
+            cout << "Observer1 recv 1 msg!" << endl;
+            break;
+        case 2:
+            cout << "Observer1 recv 2 msg!" << endl;
+            break;
+        default:
+            cout << "Observer1 recv unknow msg!" << endl;
+            break;
         }
     }
 };
@@ -51,12 +51,15 @@ class Observer2 :public Observer
     {
         switch (msgid)
         {
-            case 2:
-                cout << "Observer2 recv 2 msg!" << endl;
-                break;
-            default:
-                cout << "Observer2 recv unknow msg!" << endl;
-                break;
+        case 1:
+            cout << "Observer2 recv 1 msg!" << endl;
+            break;
+        case 3:
+            cout << "Observer2 recv 3 msg!" << endl;
+            break;
+        default:
+            cout << "Observer2 recv unknow msg!" << endl;
+            break;
         }
     }
 };
@@ -69,8 +72,8 @@ class Observer3 :public Observer
     {
         switch (msgid)
         {
-            case 1:
-                cout << "Observer3 recv 1 msg!" << endl;
+            case 2:
+                cout << "Observer3 recv 2 msg!" << endl;
                 break;
             case 3:
                 cout << "Observer3 recv 3 msg!" << endl;
